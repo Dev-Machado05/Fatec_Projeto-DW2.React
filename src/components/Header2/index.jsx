6a13b057f2b48f067a7f281c5284a26dd796882c
Lucas Machado <lucasmachadoale4@gmail.com>
2024-10-04 18:12:41 -0300

    Fix: fixed the design of the login and sign-up pages

--- src/components/Header2/index.jsx
+++ src/components/Header2/index.jsx
@@ -1,26 +1,32 @@
 import React from "react";
-import Logo from '../../assets/image/Logo1.jpeg';
-import UserIcon from '../../assets/image/user.png';
-import './style.css';
+import Logo from "../../assets/image/Logo1.jpeg";
+import UserIcon from "../../assets/image/user.png";
+import "./style.css";
 
 export default function Header2() {
-    return (
-        <div className="HeaderContainer2">
-            <div className="LogoContainer2">
-                <img src={Logo} alt="Icone da logo" />
-                <p>BuyFlow</p>
-            </div>
+  return (
+    <div className="HeaderContainer2">
+      <div className="LogoContainer2">
+        <img src={Logo} alt="Icone da logo" />
+        <p>BuyFlow</p>
+      </div>
 
-            {/* Implementar a side bar */}
-            {/* <nav>
+      <div className="SearchBar">
+        <input type="text" name="" id="" />
+        <button>
+          <img src="" alt="" />
+        </button>
+      </div>
+      {/* Implementar a side bar */}
+      {/* <nav>
                 <a href=""><strong>Home</strong></a>
                 <a href="">Compras</a>
                 <a href="">Sobre</a>
             </nav> */}
-            <div className="UserAccount">
-                <p>User</p>
-                <img src={UserIcon} alt="Imagem do usuário" />
-            </div>
-        </div>
-    )
+      <div className="UserAccount">
+        <p>User</p>
+        <img src={UserIcon} alt="Imagem do usuário" />
+      </div>
+    </div>
+  );
 }
